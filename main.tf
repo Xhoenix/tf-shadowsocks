95d8ec17bd5252c7c77be84a805c386743d87b82
Genial Hacker <86168235+GenialHacker@users.noreply.github.com>
2022-07-06 14:35:10 +0530

    Update main.tf
    Removed  redundant code and made it more inline with my blog post.

--- main.tf
+++ main.tf
@@ -6,18 +6,6 @@ terraform {
   }
 }
 
-resource "google_project_service" "project" {
-  project = var.project
-  service = "compute.googleapis.com"
-
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
-
-  disable_dependent_services = true
-}
-
 provider "google" {
   project = var.project
   region  = var.region
